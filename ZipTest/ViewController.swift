e4c17dd52a0d0e9506ce22efcacbd74663878488
Martin Troup <martin.troup@quanti.cz>
2018-04-05 18:29:54 +0200

    Testing project implemented.

---
+++ ZipTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZipTest
+//
+//  Created by Martin Troup on 05/04/2018.
+//  Copyright © 2018 quanti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
